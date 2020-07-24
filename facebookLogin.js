4ddf9ebe96f034ac5f3aa997f31d858d27666f8d
Adam McNown <adammcnown@gmail.com>
2020-07-24 15:10:20 -0400

    looking for response

--- facebookLogin.js
+++ facebookLogin.js
@@ -1,31 +1,74 @@
-//loads the SDK
-window.fbAsyncInit = function () {
-    FB.init({
-        appId: '979549989159351',
-        autoLogAppEvents: true,
-        xfbml: true,
-        version: 'v7.0'
-    });
-    FB.getLoginStatus(function (response) {
-        statusChangeCallback(response);
-    });
-};
+   //all my things I'll grab in document
+   //note this was mostly done by walking through youtube videos and i modified things were i needed too
+   //simple not good at api stuff just yet
+   let login = document.getElementById('fb-login');
+   let logot = document.getElementById('logout');
+   let welcome = document.getElementById('welcome');
 
-(function (d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {
-        return;
-    }
-    js = d.createElement(s);
-    js.id = id;
-    js.src = "https://connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+   //loads the SDK
+   window.fbAsyncInit = function () {
+       FB.init({
+           appId: '979549989159351',
+           autoLogAppEvents: true,
+           xfbml: true,
+           version: 'v7.0'
+       });
+       FB.getLoginStatus(function (response) {
+           statusChangeCallback(response);
+       });
+   };
 
-function statusChangeCallback(res) {
-    if (res.status == 'connected') {
-        console.log('Logged in and authenticated');
-    } else {
-        console.log('Not authenticated');
-    }
-}
+   (function (d, s, id) {
+       var js, fjs = d.getElementsByTagName(s)[0];
+       if (d.getElementById(id)) {
+           return;
+       }
+       js = d.createElement(s);
+       js.id = id;
+       js.src = "https://connect.facebook.net/en_US/sdk.js";
+       fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
+   function statusChangeCallback(res) {
+       if (res.status == 'connected') {
+           console.log('Logged in and authenticated');
+           log(true);
+           info();
+       } else {
+           console.log('Not authenticated');
+           log(false);
+       }
+   }
+
+   //pulls a login app for loging into facebook
+   function checkLoginState() {
+       FB.getLoginStatus(function (response) {
+           statusChangeCallback(response);
+       });
+   }
+
+   function log(isLoggedIn) {
+       if (isLoggedIn) {
+           logot.setAttribute('style', 'display: block;');
+           login.setAttribute('style', 'display: none;');
+           welcome.textContent = 'Hello ';
+       } else {
+           logot.setAttribute('style', 'display: none;');
+           login.setAttribute('style', 'display: block;');
+           welcome.textContent = 'Welcome';
+       }
+   }
+
+   function info() {
+       FB.api('/me?field=name', function (response) {
+           if (response && !response.error) {
+               console.log(response);
+           }
+       });
+   }
+
+   function logout() {
+       FB.logout(function (response) {
+           log(false);
+       });
+   }
