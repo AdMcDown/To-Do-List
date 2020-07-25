896170b456d54cfef5c403256016cf2ba518e1ec
Adam McNown <adammcnown@gmail.com>
2020-07-25 16:07:01 -0400

    removes list items if logout

--- facebookLogin.js
+++ facebookLogin.js
@@ -62,8 +62,16 @@
            welcome.textContent = '\'Welcome\' - Please Login';
 
            //added this to empty the ordered lists when you logout
-           let allOl = document.querySelectorAll('ol');
-
+           let olList = document.querySelectorAll('ol');
+           console.log(olList);
+           for (let o = 0; o < olList.length; o++) {
+               let liList = olList[o].getElementsByTagName("li");
+               for (let l = 0; l < liList.length; l++) {
+                   console.log(liList[l]);
+                   liList[l].remove();
+               }
+               console.log(olList[o]);
+           }
        }
    }
 
